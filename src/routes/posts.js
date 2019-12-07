6660e4d101f6fc6282c1ea97b24c59ff8a9e44db
Edson Junio Malaquias <30880842+EdsonJunioMalaquias@users.noreply.github.com>
2019-12-07 11:57:05 -0300

    created put methd in post

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -8,7 +8,8 @@ router.get('/', PostController.findAllPosts);
 router.get('/:_id', PostController.findPostByID);
 router.post('/', PostController.createPost);
 router.delete('/:_id', PostController.DeletePost);
-/*router.put('/:_id', PostController.updatePost);
+router.put('/:_id', PostController.updateOne);
+/*;
 
 router.patch('/:_id', PostController.UpdateOneField);
 router.get('/:_id/comments', PostController.findAllPostByID);*/
