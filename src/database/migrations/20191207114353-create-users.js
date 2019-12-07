fdcb201e2a69dc50d1a4e3fbf66a36d80d19ea63
Edson Junio Malaquias <30880842+EdsonJunioMalaquias@users.noreply.github.com>
2019-12-07 09:01:52 -0300

    Create Migrate create-users

---
+++ src/database/migrations/20191207114353-create-users.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        isEmail: true,
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      website: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        isUrl: true,
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
